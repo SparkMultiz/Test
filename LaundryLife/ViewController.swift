4068fee15de9a313b5bcf2a1e95fa088bff5e4e0
Ilesh Panchal <ilesh.panchal1991@gmail.com>
2018-07-21 11:13:14 +0530

    Initial Commit

---
+++ LaundryLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaundryLife
+//
+//  Created by Chirag Patel on 7/21/18.
+//  Copyright © 2018 Spark.multiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
